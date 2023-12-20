227523ab4ffcfd21eb0fa1d8c76342c869b0dc55
Vladimir Kvachevskii <mc@mcsimple.net>
2023-12-20 13:35:18 +0400

    updated apk

--- app/src/main/java/com/improve/weatherapp/WeatherAPI.java
+++ app/src/main/java/com/improve/weatherapp/WeatherAPI.java
@@ -6,10 +6,10 @@ import retrofit2.http.Query;
 
 public interface WeatherAPI {
 
-    @GET("weather?appid=ccaf1b3c554ff9521df4fa82b0a324b2&units=metric")
+    @GET("weather?appid=*************=metric")
     Call<OpenWeatherMap>getWeatherWithLocation(@Query("lat")double lat,@Query("lon")double lon);
 
-    @GET("weather?appid=ccaf1b3c554ff9521df4fa82b0a324b2&units=metric")
+    @GET("weather?appid=**************&units=metric")
     Call<OpenWeatherMap>getWeatherWithCityName(@Query("q")String city);
 
 }
